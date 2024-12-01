d9f5e5e53766cbfb898cf4471793b417a2a61357
RenatoFleury <renatonavesfleury@gmail.com>
2024-12-01 18:13:55 -0300

    adicionado outras consultas

--- functions.py
+++ functions.py
@@ -13,9 +13,18 @@ def mes_para_numero(nome_mes):
         None: Caso o nome do mês não seja válido.
     """
     meses = {
-        "janeiro": 1, "fevereiro": 2, "março": 3, "abril": 4, 
-        "maio": 5, "junho": 6, "julho": 7, "agosto": 8, 
-        "setembro": 9, "outubro": 10, "novembro": 11, "dezembro": 12
+        "janeiro": 1, "jan": 1,
+        "fevereiro": 2, "fev": 2,
+        "março": 3, "mar": 3,
+        "abril": 4, "abr": 4,
+        "maio": 5, "mai": 5,
+        "junho": 6, "jun": 6,
+        "julho": 7, "jul": 7,
+        "agosto": 8, "ago": 8,
+        "setembro": 9, "set": 9,
+        "outubro": 10, "out": 10,
+        "novembro": 11, "nov": 11,
+        "dezembro": 12, "dez": 12
     }
     
     # Converter para minúsculas para lidar com nomes maiúsculos ou mistos
