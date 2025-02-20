8e926f64d8dcc8ef7d2848c5c06683e6020e3042
RenatoFleury <renatonavesfleury@gmail.com>
2025-02-20 18:18:30 -0300

    test

--- pages/ccr_diagnosticos.py
+++ pages/ccr_diagnosticos.py
@@ -5,6 +5,36 @@ import plotly.express as px
 import plotly.graph_objects as go
 from utils.functions import mes_para_numero, categorize_period
 
+def ccr_diagnosticos_first_graph():
+    csv_file = 'csv_files/ccr_diagnostico.csv'
+    origin_df = pd.read_csv(csv_file, sep=';')
+    columns = list(origin_df.columns)
+
+    formated_df = pd.DataFrame()
+    formated_df["Data"] = origin_df[columns[0]].apply(lambda x: x.split("/")[1]+'/'+str(mes_para_numero(x.split("/")[0])))
+    formated_df["Data"] = pd.to_datetime(formated_df["Data"], format='%Y/%m')
+    formated_df["Ano"] = formated_df["Data"].apply(lambda x: x.year)
+    formated_df["Mês"] = formated_df["Data"].apply(lambda x: x.month)
+    formated_df["Período"] = formated_df["Ano"].apply(categorize_period)
+    for column in columns[1:]:
+        formated_df[column] = origin_df[column]
+        
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df["1 Região Norte"], mode='lines', name='1 Região Norte'))
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df["2 Região Nordeste"], mode='lines', name='2 Região Nordeste'))
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df["3 Região Sudeste"], mode='lines', name='3 Região Sudeste'))
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df["4 Região Sul"], mode='lines', name='4 Região Sul'))
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df["5 Região Centro-Oeste"], mode='lines', name='5 Região Centro-Oeste'))
+    fig.add_trace(go.Scatter(x=formated_df["Data"], y=formated_df[" Total"], mode='lines', name='Total'))
+    fig.update_layout(
+        title=dict(text="DIAGNOSTICOS DE CÂNCER COLORRETAL"),
+        xaxis_title='Data',
+        yaxis_title='Quantidade',
+        plot_bgcolor='white',
+    )
+    
+    st.plotly_chart(fig)
+
 def main():
     NavBar()
     
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from components.navbar import NavBar
+from pages.ccr_diagnosticos import ccr_diagnosticos_first_graph
 
 def main():
 
     NavBar()
     
-    st.title('Exploratory Data Analysis - Data SUS')
+    st.header('Exploratory Data Analysis - Data SUS')
     
     st.write('Este é um projeto de EDA utilizando a base de dados do Data SUS. O objetivo é analisar os dados de diagnósticos de câncer colorretal e a quantidade de exames realizados por região e período. Os dados utilizados estão disponíveis no site do Data SUS.')
    
     st.write('Acesse o código fonte do projeto no [GitHub](https://github.com/RenatoFleury/eda_data_sus)')
+    
+    st.subheader('Overview dos dados analisados')
+    ccr_diagnosticos_first_graph()
 
 
 if __name__ == '__main__':
